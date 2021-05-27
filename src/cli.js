290c0a566c8572732c45d4915056d4bae3c57dac
Red Williams <red.devcs@gmail.com>
2021-05-27 17:04:04 -0400

    scrub scrub

--- src/cli.js
+++ src/cli.js
@@ -75,6 +75,9 @@ inquirer
       case 4:
         console.log("Exiting");
         return;
+
+      default:
+        return;
     }
   })
   .catch((error) => {
@@ -128,65 +131,72 @@ async function handlePUT() {
     .then(async (answers) => {
       // If requesting confirmation
       if (!program.opts().yes) {
+        // Print form
         console.log("\n", answers, "\n");
 
         // Confirm
         if (!(await confirm())) return;
       }
+
+      // Submit request
       putDB(answers);
     })
     .catch((error) => {
       if (error.isTtyError) {
         // Prompt couldn't be rendered in the current environment
+        console.log("The prompt cannot be rendered in the current environment");
       } else {
         // Something else went wrong
-        console.log("Here");
+        console.log("Something went wrong");
+        console.log(error);
       }
     });
 }
 
 async function handlePATCH() {
-  inquirer.prompt([
-    /* Pass your questions in here */
-    {
-      type: "input",
-      message: "Enter the project name:",
-      name: "project_name",
-    },
-    {
-      type: "input",
-      message: "Enter the project desription:",
-      name: "project_description",
-    },
-    {
-      type: "input",
-      message: "Enter the tech list:",
-      name: "tech_list",
-      suffix: " (comma-separated values)",
-      filter: (input) => {
-        input = input.split(",");
-        for (var i = 0; i < input.length; i++) {
-          input[i] = input[i].trim();
-        }
-        return input;
+  inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
+        type: "input",
+        message: "Enter the project name:",
+        name: "project_name",
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Github link:",
-      name: "github_link",
-    },
-    {
-      type: "input",
-      message: "Enter the project link:",
-      name: "project_link",
-    },
-    {
-      type: "confirm",
-      message: "Is this a featured project?",
-      name: "featured",
-    },
-  ]);
+      {
+        type: "input",
+        message: "Enter the project desription:",
+        name: "project_description",
+      },
+      {
+        type: "input",
+        message: "Enter the tech list:",
+        name: "tech_list",
+        suffix: " (comma-separated values)",
+        filter: (input) => {
+          input = input.split(",");
+          for (var i = 0; i < input.length; i++) {
+            input[i] = input[i].trim();
+          }
+          return input;
+        },
+      },
+      {
+        type: "input",
+        message: "Enter the Github link:",
+        name: "github_link",
+      },
+      {
+        type: "input",
+        message: "Enter the project link:",
+        name: "project_link",
+      },
+      {
+        type: "confirm",
+        message: "Is this a featured project?",
+        name: "featured",
+      },
+    ])
+    .then((answers) => console.log(answers));
 }
 
 /* --------------------------- API Request Methods -------------------------- */
